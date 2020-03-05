8425449098443097144fc16bde44b763f41a9879
Kelsey1234 <49014666+Kelsey1234@users.noreply.github.com>
2020-03-04 19:38:17 -0600

    put

--- fotos.js
+++ fotos.js
@@ -15,11 +15,19 @@ router.get('/users/:id',(req,res)=>{
     return res.status(200).json(user);
 
 });
+
 router.post('/users/new', (req,res)=>{
     var datosEnviados = req.body;
     var newUser = userModel.addNew(datosEnviados);
     return res.status(200).json(newUser);
 });
+
+router.put('/users/upd/:id', (req, res)=>{
+    var id = parseInt(req.params.id);
+    var updUser = userModel.update( id, req.body);
+    return res.status(200).json(updUser);
+});
+
 
 
 module.exports = router;
